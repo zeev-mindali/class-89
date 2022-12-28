24371c9267cbd645e85f3414c7c6f18f5fec1fbc
zeev mindali <mindalizeev@gmail.com>
2022-12-28 11:35:12 +0200

    input

--- JavaScript/js_28_12/targil_cars/js/cars.js
+++ JavaScript/js_28_12/targil_cars/js/cars.js
@@ -3,5 +3,34 @@ const sayHi = () => {
 };
 
 const handleData = () => {
+  //pointers
+  const carNumber = document.getElementById("carNumber").value;
+  const carType = document.getElementById("carType").value;
+  const carFuels = document.getElementsByName("carFuel");
+  const carYear = +document.getElementById("year").value;
+  const carLic = document.getElementById("license").checked;
+
+  var fuel = "";
+  carFuels.forEach((item) => {
+    if (item.checked) {
+      fuel = item.value;
+    }
+  });
+
+  console.log(carNumber, carType, fuel, carYear, carLic);
+};
+
+const handleData_old = () => {
   //   alert("אתה ממש קרציה");
+  const carFuels = document.getElementsByName("carFuel");
+  var carFuel = "";
+  for (var index = 0; index < carFuels.length; index++) {
+    if (carFuels[index].checked) {
+      //console.log(carFuels[index].value);
+      carFuel = carFuels[index].value;
+      break;
+    }
+    //console.log(index);
+  }
+  console.log(carFuel);
 };
