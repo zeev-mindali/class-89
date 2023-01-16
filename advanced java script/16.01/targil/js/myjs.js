c198cc120a8fe8c85a5c7e9f422b989580f17acf
zeev mindali <mindalizeev@gmail.com>
2023-01-16 15:55:55 +0200

    targil2

--- advanced java script/16.01/targil/js/myjs.js
+++ advanced java script/16.01/targil/js/myjs.js
@@ -26,3 +26,26 @@ console.log("מבצע וודקה + 10 רדבול".makeStar());
 // 5 => 5
 // 10000 => 10,000
 
+
+//add a function to Number with prototype....
+Number.prototype.makeComma = function () {
+    //45678=>45,678
+    //insert the number into temp variable
+    let temp = this;
+    //make the result variable empty and from string type
+    let result = "";
+    //continue as long temp is more then 0
+    while (temp>0){
+        //get last 3 digits and add comma (,) if result is not empty
+        result = temp % 1000 + (result.length>1?",":"")+ result;
+        //take out the 3 numbers that we used
+        temp = Number.parseInt(temp/1000);
+    }
+    //return result.substring(0,result.length-1);
+
+    //return the result.
+    return result;
+}
+
+let myNumber = 1234567890;
+console.log(myNumber.makeComma())
