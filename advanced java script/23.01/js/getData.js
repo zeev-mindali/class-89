ce7c0934ea1215de0f8f6f93eb84dc24d83a0980
zeev mindali <mindalizeev@gmail.com>
2023-01-23 10:29:44 +0200

    car locator

--- advanced java script/23.01/js/getData.js
+++ advanced java script/23.01/js/getData.js
@@ -6,11 +6,15 @@ const getCarTruck = async (carID) => {
     );
 };
 
+const getHandiCap = async (carID) => {
+  return await fetch(urlHandiCap + carID)
+    .then((response) => response.json())
+    .then((data) => data.result.records.length > 0);
+};
+
 const getBike = async (carID) => {};
 
 const getTruck = async (carID) => {};
-
-const getHandiCap = async (carID) => {};
 
 const getRecall = async (carID) => {};
 
--- advanced java script/23.01/js/getInfo.js
+++ advanced java script/23.01/js/getInfo.js
@@ -6,11 +6,14 @@
 //getOffroad
 
 const locateCar = async () => {
+  //get car id from input text box
   const carID = document.getElementById("carNumber").value;
+  //check if we have a car...
   await getCarTruck(carID)
     .then((data) => {
       if (data != null) {
         //we have a car, check for handicap
+        let isHandiCap = await getHandiCap(carID);
       } else {
         //check truck or bike
       }
@@ -25,4 +28,5 @@ const locateCar = async () => {
         //check if it offroad
       })
     );
+    console.log("me here")
 };
