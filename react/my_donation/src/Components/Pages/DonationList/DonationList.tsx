ba0b168eaa2d6bcd85d6388c115ed3c902c93497
zeev mindali <mindalizeev@gmail.com>
2023-03-08 14:10:51 +0200

    delete

--- react/my_donation/src/Components/Pages/DonationList/DonationList.tsx
+++ react/my_donation/src/Components/Pages/DonationList/DonationList.tsx
@@ -1,12 +1,30 @@
 import Donation from "../../../models/donation";
 import "./DonationList.css";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function DonationList(): JSX.Element {
-  const donationList:Donation[] = JSON.parse(localStorage.getItem("userDonation"));
+  /*const donationList:Donation[] = JSON.parse(localStorage.getItem("userDonation"));*/
+  const [donationList,setList] = useState<Donation[]>([]);
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    setList(JSON.parse(localStorage.getItem("userDonation")));
+  },[]);
+
   //console.log("donation list: ",donationList);
 
   const getDate = (timeStamp:number)=>{
     return new Date(timeStamp).toLocaleString(); //convert timestamp 5746387832=>local date 08/03/2023
+  }
+
+  const deleteDonation = (timeStamp:number)=>{
+    //1,2,3,4,5  filter=>item !=5
+    //1,2,3,4
+    const newList = donationList.filter(item=>item.timeStamp!=timeStamp);
+
+    localStorage.setItem("userDonation", JSON.stringify(newList)); 
+    setList(newList);
   }
 
   return (
@@ -15,12 +33,14 @@ function DonationList(): JSX.Element {
       <hr />
       <table>
           <thead>
+            <tr>
             <th>Date</th>
             <th>Name</th>
             <th>sum</th>
             <th>memo</th>
             <th>edit</th>
             <th>delete</th>
+            </tr>
           </thead>
           <tbody>
             {donationList.map(donation=>
@@ -29,8 +49,8 @@ function DonationList(): JSX.Element {
               <td>{donation.donarName}</td>
               <td>{donation.donarSum}</td>
               <td>{donation.donarMemo}</td>
-              <td>✏️</td>
-              <td>❌</td>
+              <td onClick={()=>navigate("/info")}>✏️</td>
+              <td onClick={()=>deleteDonation(donation.timeStamp)}>❌</td>
             </tr>
             )}
           </tbody>
