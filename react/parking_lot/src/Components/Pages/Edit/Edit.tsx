579c11ef23f782b5c1c7ef89f58b2f5be1f854bd
zeev mindali <mindalizeev@gmail.com>
2023-03-13 12:05:03 +0200

    cars

--- react/parking_lot/src/Components/Pages/Edit/Edit.tsx
+++ react/parking_lot/src/Components/Pages/Edit/Edit.tsx
@@ -6,13 +6,28 @@ import "./Edit.css";
 function Edit(): JSX.Element {
   const navigate = useNavigate();
   const params = useParams();
-  const [carData, setData] = useState<Car>();
+  const tempCar = new Car("na", "na", "na", 0, 0, 0, 0);
+  const [carData, setData] = useState<Car>(tempCar);
 
   useEffect(() => {
-    const allData = localStorage.getItem("parking");
-    
+    const allData = JSON.parse(localStorage.getItem("parking"));
+    const parkingNumber = +params.parkingNumber;
+    const singleData = allData.filter(
+      (item: Car) => item.parkingNumber === parkingNumber
+    );
+    //console.log(singleData);
+    //setData(singleData);
   }, []);
-  return <div className="Edit"></div>;
+
+//carNum,carOwner,ownerPhone,ownerFlat,ownerCars,ownerBuilding,parkingNumber
+
+  return (
+    <div className="Edit Box">
+      <h2>{carData.parkingNumber}</h2>
+      <hr />
+      
+    </div>
+  );
 }
 
 export default Edit;
