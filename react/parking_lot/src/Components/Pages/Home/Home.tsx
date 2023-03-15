e3e023b300b665a8d70d7a8e0bc6dd55fb6bd48d
zeev mindali <mindalizeev@gmail.com>
2023-03-15 10:53:20 +0200

    notify

--- react/parking_lot/src/Components/Pages/Home/Home.tsx
+++ react/parking_lot/src/Components/Pages/Home/Home.tsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import notify from "../../Utils/Notify";
 import "./Home.css";
 
 function Home(): JSX.Element {
+    const navigate = useNavigate();
+  useEffect(()=>{
+    if (localStorage.getItem("token") === null || localStorage.getItem("token").length<10){
+        //no login made yet
+        notify.error("Please Login");
+        navigate("/login");
+    }
+  },[])  
   return (
     <div className="Home">
       <h1>we will put here a picture and some info</h1>
--- react/parking_lot/src/Components/Pages/Login/Login.tsx
+++ react/parking_lot/src/Components/Pages/Login/Login.tsx
@@ -1,9 +1,25 @@
+import { AccountBox, Password } from "@mui/icons-material";
+import { Button, ButtonGroup, Checkbox, TextField, Typography } from "@mui/material";
+import notify from "../../Utils/Notify";
 import "./Login.css";
 
 function Login(): JSX.Element {
     return (
-        <div className="Login">
-			
+        <div className="Login Box" style={{marginTop:150}}>
+			<Typography variant="h4" className="HeadLine">Login</Typography>
+            <hr/><br/><br/>
+            <AccountBox style={{fontSize:40, margin:10}}/>
+            <TextField label="user name" variant="outlined"/>
+            <br/><br/>
+            <Password style={{fontSize:40, margin:10}}/>
+            <TextField label="Password" type="password" variant="outlined"/>
+            <br/><br/>
+            <Checkbox/> <label>Remmber me</label>
+            <br/><br/>
+            <ButtonGroup variant="contained" fullWidth>
+                <Button color="primary" onClick={()=>{notify.success("Matan is in the house")}}>Login</Button>
+                <Button color="secondary">Register</Button>
+            </ButtonGroup>
         </div>
     );
 }
