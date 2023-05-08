147687e83d24fdc702b850e0e6388c3227c00b9e
zeev mindali <mindalizeev@gmail.com>
2023-05-08 10:57:40 +0300

    first angular

---
+++ angular/hello_angular/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'hello_angular';
+  matan = 'Matan the king of the world';
+}
