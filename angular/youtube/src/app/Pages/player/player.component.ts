393aa9b8da33c591175f1c5292b613ed7322a3ad
zeev mindali <mindalizeev@gmail.com>
2023-05-22 11:15:56 +0300

    youtube error

--- angular/youtube/src/app/Pages/player/player.component.ts
+++ angular/youtube/src/app/Pages/player/player.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.css']
+  styleUrls: ['./player.component.css'],
 })
-export class PlayerComponent {
+export class PlayerComponent implements OnInit {
+  id: any = 'error';
+  playerURL = 'https://www.youtube.com/embed/U6XcNpYQ_zI';
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+    console.log('https://youtu.be/' + this.id);
+    //this.playerURL = 'https://www.youtube.com/embed/' + this.id;
+  }
 }
